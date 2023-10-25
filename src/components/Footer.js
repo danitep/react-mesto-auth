0fa8b4c20526a51004bd03c9304f4a1b636b744d
danitep <129656824+danitep@users.noreply.github.com>
2023-10-25 13:01:44 +0500

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-    return(
-    <footer className="footer">
-      <p className="footer__text">© {new Date().getFullYear()} Mesto Russia</p>
-    </footer>
-    )
-}
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import logoPath from '../images/mesto_logo.svg';
-
-export default function Header(){
-    return(
-    <header className="header">
-      <img className="header__logo" src={logoPath} alt="логотип"/>
-    </header>
-    )
-}
